69ea6b0060e86cc7274cbc3b2da79bc570ed9b15
Yasir Abdulwahab <dev.yassersa@gmail.com>
2020-12-02 03:37:36 +0300

    social icons, meta & social

---
+++ social.js
@@ -0,0 +1,18 @@
+{
+    "@context": "http://schema.org",
+    "@type": "Company",
+    "name": "حلول ميسان لتقنية المعلومات",
+    "logo": "https://raw.githubusercontent.com/mryassera/mryassera.github.io/main/og-img.jpg",
+    "url": "https://www.hloolmysan.com",
+    "sameAs": [
+        "https://twitter.com/hloolmysan",
+        "https://www.instagram.com/hloolmysan?hl=ar",
+        "https://www.snapchat.com/add/hloolmysan"
+    ],
+    "email": "hloolmysan@gmail.com",
+    "address": {
+        "@type": "PostalAddress",
+        "addressRegion": "الرياض",
+        "addressCountry": "SA"
+    }
+}
